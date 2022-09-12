7f8a1a76ffd3b5271129087d142d398b194b0144
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-09-13 01:17:03 +0300

    Create: - PageTitle component - EventBoard component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import {PageTitle} from "./PageTitle/PageTitle";
+import {EventBoard} from "./EventBoard/EventBoard";
+import upcomingEvents from '../upcoming-events.json'
+
+export function App() {
+  return(
+      <div>
+        <PageTitle text = '24th Core Worlds Coalition Conference' />
+        <EventBoard events={ upcomingEvents } />
+      </div>
+  )
+}
+
+export default App;
