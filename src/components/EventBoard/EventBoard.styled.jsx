cfe84f69c207bb15842dedb1834ff2171ac95e21
Anton-Tretiak <tretiak.ap@gmail.com>
2022-10-08 16:02:51 +0300

    feat: add emotion library and modify some files

---
+++ src/components/EventBoard/EventBoard.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Board = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(240px, auto));
+  gap: 24px;
+  padding-left: 200px;
+  padding-right: 200px;
+  background-color: rgb(227, 230, 235);
+  padding-top: 50px;
+  padding-bottom: 50px;
+  margin: 0;
+`
